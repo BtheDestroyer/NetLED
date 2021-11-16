a89635dc0ec0d16f153d5e23a583b8c77c94ed38
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2021-11-16 03:04:42 -0800

    Update led.py

--- led.py
+++ led.py
@@ -6,23 +6,23 @@ config = cfg.config["led"]
 initialized = False
 strip = None
 
-def init():
-    log.info("Initializing LED strip")
-    if is_initialized():
-        log.warning("Strip already initialized. Reinitializing")
-    strip = rpi_ws281x.PixelStrip(
-        config["count"],
-        config["pin"],
-        config["freq"],
-        config["dma"],
-        config["invert"],
-        config["brightness"],
-        config["channel"])
-    if strip is None:
-        log.error("Failed to construct led strip!")
-        return
+log.info("Initializing LED strip")
+if is_initialized():
+    log.warning("Strip already initialized. Reinitializing")
+strip = rpi_ws281x.PixelStrip(
+    config["count"],
+    config["pin"],
+    config["freq"],
+    config["dma"],
+    config["invert"],
+    config["brightness"],
+    config["channel"])
+if strip is None:
+    log.error("Failed to construct LED strip!")
+else:
     strip.begin()
     initialized = True
+    log.info("Initialized LEDs!")
 
 def is_initialized():
     return initialized 
@@ -30,7 +30,6 @@ def is_initialized():
 def set_pixel(index : int, color : rpi_ws281x.Color, show : bool = False):
     if not is_initialized():
         log.error("Tried to set color of pixel when strip is not initialized")
-        log.error("Call `led.init()` first!")
         return
     if index >= config["count"]:
         log.error("Tried to set color of pixel %d when the strip is only %d pixels long" % (index, config["count"]))
@@ -39,4 +38,7 @@ def set_pixel(index : int, color : rpi_ws281x.Color, show : bool = False):
         strip.show()
 
 def update():
+    if not is_initialized():
+        log.error("Tried to update when strip is not initialized")
+        return
     strip.show()
